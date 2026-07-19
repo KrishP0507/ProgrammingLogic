a56eb6e3f06fd7bf62a822c1e8b4d42951f6c9c0
KrishP0507 <s01385653@acad.tri-c.edu>
2026-07-19 03:20:48 +0000

    Added Lab3

--- Lab1/obj/Debug/net10.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net10.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6faf7b1801731fdd54ed2108e75a087db2006eb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93834116caecc256f2e790c8388fac7dd24bc612")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
